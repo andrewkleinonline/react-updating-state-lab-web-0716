ba03207b63e8471c1e108b5784e6fa852ac4cf29
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-25 16:32:04 -0400

    complete lab

---
+++ components/DigitalClicker.js
@@ -0,0 +1,26 @@
+const React = require('react');
+
+class DigitalClicker extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  render() {
+    return(
+      <button onClick={() => {
+          this.setState({timesClicked: this.state.timesClicked + 1})
+        }
+      }>{this.state.timesClicked}</button>
+    )
+  }
+
+
+}
+
+
+
+
+module.exports = DigitalClicker;
